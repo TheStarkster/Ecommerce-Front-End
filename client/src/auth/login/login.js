f46ab34f6ec31a08233cc434139e3375c1499a7b
Gurkaran Singh <sgurkaran2000@gmail.com>
2019-10-20 04:16:36 +0530

    Loading Animation

---
+++ client/src/auth/login/login.js
@@ -0,0 +1,88 @@
+import React, { Component } from 'react';
+import '../styles.css'
+import axios from 'axios'
+
+class Login extends Component {
+
+    constructor() {
+        super()
+        this.state = {
+            password: '',
+            email: '',
+            Loading: 'none'
+        }
+        this.handlePasswordChange = (event) => {
+            this.setState({
+                password: event.target.value
+            })
+        }
+        this.handleEmailChange = (event) => {
+            this.setState({
+                email: event.target.email
+            })
+        }
+        this.SubmitHandler = () => {
+            this.setState({
+                Loading: 'block'
+            })
+            // axios.post('http://3.87.22.103:2024/signin',{
+            //     email:this.state.email,
+            //     pass:this.state.password
+            // })
+            // .then(response => {
+            //     if(response.data.message === "200: User Authenticated"){
+
+            //     }
+            // })
+        }
+    }
+
+
+
+    render() {
+        return (
+            <div>
+                <div className="Loading-Root" style={{
+                    display: this.state.Loading,
+                    width: "100%",
+                    height: "100%",
+                    position: "fixed",
+                    backgroundColor: "#00000021",
+                    zIndex: '99',
+                }}>
+                    <img src={require('../loading.gif')} alt="Loading" style={{
+                        position: "fixed",
+                        top: "50%",
+                        left: "50%",
+                        transform: "translate(-50%,-50%)",
+
+                    }}></img>
+                </div>
+                <div className="Register-Root">
+                    <div className="Logo">
+                        <div className="panel-Dental">Dental</div>
+                        <div className="panel-Stall">Stall</div>
+                    </div>
+                    <h2>Please Enter Credentials</h2>
+                    <h5>To Sign In</h5>
+                    <div className="form-field">
+                        <input type="email"
+                            className="email-input"
+                            placeholder="Email"
+                            onChange={this.handleEmailChange} />
+                    </div>
+                    <div className="form-field">
+                        <input type="password"
+                            className="password-input"
+                            placeholder="Password"
+                            onChange={this.handlePasswordChange} />
+                    </div>
+                    <button className="submit-input" onClick={this.SubmitHandler}>Sign In</button>
+                    <button className="Redirect-input">Sign Up</button>
+                </div>
+            </div>
+        );
+    }
+}
+
+export default Login;
--- client/src/routes/paths.js
+++ client/src/routes/paths.js
@@ -1,9 +1,8 @@
 import React, { Component } from 'react';
 import { Route, Switch } from 'react-router-dom';
 import FourOFour from '../404'
-import Login from '../login'
+import Login from '../auth/login/login'
 import Register from '../auth/register/register'
-
 class Paths extends Component {
     render() {
         return (
@@ -13,7 +12,6 @@ class Paths extends Component {
                     <Route path="/SignIn" component={Login} />
                     <Route path="/Register" component={Register} />
                     <Route path="/SignUp" component={Register} />
-                    
                     <Route path="*" component={FourOFour} />
                 </Switch>
             </div>
