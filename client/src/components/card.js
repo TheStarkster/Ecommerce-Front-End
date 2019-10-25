2ae78083c937aa781aae80867853fe97211185b9
Gurkaran Singh <sgurkaran2000@gmail.com>
2019-10-25 21:17:46 +0530

    Home Page

--- client/src/components/card.js
+++ client/src/components/card.js
@@ -5,12 +5,12 @@ export default class Card extends Component {
     render() {
         return (
             <div className="card">
-                <img src="https://media.dentalkart.com/catalog/product/m/i/micro_mega_one_flare_1.jpg" alt="Product"></img>
-                <h3>Product Name</h3>
-                <h5 style={{color:"grey"}}>Product Brief</h5>
+                <img src={this.props.ProductImage} alt="Product"></img>
+                <h3>{this.props.ProductName}</h3>
+                <h5 style={{color:"grey"}}>{this.props.ProductDescription}</h5>
                 <div className="container">
-                    <h5>Price</h5>
-                    <span className="Stock-Status-Span">Stock</span>
+                    <h5 className="Card-Price">Rs.{this.props.ProductPrice}</h5>
+                    <span className="Stock-Status-Span">In Stock</span>
                 </div>
             </div>
         )
--- client/src/components/home.js
+++ client/src/components/home.js
@@ -4,16 +4,51 @@ import BrandStrip from './brandstrip'
 import SearchBar from './searchbar'
 import Navbar from './navbar'
 import '../dist/styles/css/home.css'
+import axios from 'axios'
 
 export default class Home extends Component {
+    constructor(props) {
+        super(props)
+        this.state = {
+            ProductsArray: []
+        }
+        this.componentWillMount = () => {
+            axios.get('http://localhost:2024/User-fetch-products')
+                .then(response => {
+                    this.setState({
+                        ProductsArray: [...response.data.products]
+                    })
+                })
+        }
+    }
+    RenderProductCards = () => {
+        this.ProductCard = []
+        this.state.ProductsArray.forEach(element => {
+            this.ProductCard.push(
+                <Card ProductName={element.name}
+                    ProductPrice={element.price}
+                    ProductBrand={element.brand}
+                    ProductKeyFeatures={element.KeyFeatures}
+                    ProductPrimaryFeatures={element.PrimaryFeatures}
+                    ProductDescription={element.disc}
+                    ProductImage={element.image}
+                    ProductMrp={element.mrp}
+                    ProductRDL={element.rdl}
+                    key={element._id}
+                >
+                </Card>
+            )
+        });
+        return this.ProductCard
+    }
     render() {
         return (
             <div>
                 <Navbar history={this.props.history}></Navbar>
                 <div className="Poster">
                     <h1 style={{
-                        backgroundColor:'#18c0c961',
-                        fontFamily:'Quicksand-Regular'
+                        backgroundColor: '#18c0c961',
+                        fontFamily: 'Quicksand-Regular'
                     }}>
                         Welcome To <br /><i>DentalStall</i><br />
                         Your Solgan Goes Here!<br />
@@ -23,17 +58,13 @@ export default class Home extends Component {
                 <SearchBar></SearchBar>
                 <h2 style={{
                     position: 'absolute',
-                    left: '50%',
+                    left: '50%', 
                     transform: 'translate(-50%,0%)',
                     fontFamily: 'Quicksand-Regular',
                     color: '#868686',
                 }}>Top Products</h2>
                 <div className="Home-Root">
-                    <Card></Card>
-                    <Card></Card>
-                    <Card></Card>
-                    <Card></Card>
-                    <Card></Card>
+                    {this.RenderProductCards()}
                 </div>
                 <div className="mobile-cart">
                     <img src={require('../dist/assets/icons/icons8-shopping-cart-48.png')} alt="cart"></img>
