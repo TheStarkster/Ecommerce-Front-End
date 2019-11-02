53c89366b8e96ecec373cbf1c253db7081b89d1f
Gurkaran Singh <sgurkaran2000@gmail.com>
2019-11-02 17:44:17 +0530

    breandstrip

--- client/src/components/brandstrip.js
+++ client/src/components/brandstrip.js
@@ -6,44 +6,44 @@ export default class BrandStrip extends Component {
         return (
             <div className="Brand-Container">
                 <h2 style={{
-                    position:'absolute',
-                    left:'50%',
-                    transform:'translate(-50%,0%)',
+                    position: 'absolute',
+                    left: '50%',
+                    transform: 'translate(-50%,0%)',
                     fontFamily: 'Quicksand-Regular',
-                    color:'white'
+                    color: 'white'
                 }}>Top Brands</h2>
                 <div className="brand">
                     <img src="https://media.dentalkart.com/brands/3M ESPE.jpg" alt="Brand"></img>
                 </div>
                 <div className="brand">
-                    <img src="https://media.dentalkart.com/brands/3M ESPE.jpg" alt="Brand"></img>
+                    <img src="https://media.dentalkart.com/brands/waldent.png" alt="Brand"></img>
                 </div>
                 <div className="brand">
-                    <img src="https://media.dentalkart.com/brands/3M ESPE.jpg" alt="Brand"></img>
+                    <img src="https://media.dentalkart.com/brands/woodpecker.png" alt="Brand"></img>
                 </div>
                 <div className="brand">
-                    <img src="https://media.dentalkart.com/brands/3M ESPE.jpg" alt="Brand"></img>
+                    <img src="https://media.dentalkart.com/brands/dentsply.jpg" alt="Brand"></img>
                 </div>
                 <div className="brand">
-                    <img src="https://media.dentalkart.com/brands/3M ESPE.jpg" alt="Brand"></img>
+                    <img src="https://media.dentalkart.com/brand/u/u/uu-ortho.jpg" alt="Brand"></img>
                 </div>
                 <div className="brand">
-                    <img src="https://media.dentalkart.com/brands/3M ESPE.jpg" alt="Brand"></img>
+                    <img src="https://media.dentalkart.com/brands/gc.png" alt="Brand"></img>
                 </div>
                 <div className="brand">
-                    <img src="https://media.dentalkart.com/brands/3M ESPE.jpg" alt="Brand"></img>
+                    <img src="https://media.dentalkart.com/brands/iDENTical.jpg" alt="Brand"></img>
                 </div>
                 <div className="brand">
-                    <img src="https://media.dentalkart.com/brands/3M ESPE.jpg" alt="Brand"></img>
+                    <img src="https://media.dentalkart.com/brand/m/a/mani-2.jpg" alt="Brand"></img>
                 </div>
                 <div className="brand">
-                    <img src="https://media.dentalkart.com/brands/3M ESPE.jpg" alt="Brand"></img>
+                    <img src="https://media.dentalkart.com/brands/Baot.jpg" alt="Brand"></img>
                 </div>
                 <div className="brand">
-                    <img src="https://media.dentalkart.com/brands/3M ESPE.jpg" alt="Brand"></img>
+                    <img src="https://media.dentalkart.com/brand/c/h/chinese.jpg" alt="Brand"></img>
                 </div>
                 <div className="brand">
-                    <img src="https://media.dentalkart.com/brands/3M ESPE.jpg" alt="Brand"></img>
+                    <img src="https://media.dentalkart.com/brands/detax.png" alt="Brand"></img>
                 </div>
             </div>
         )
